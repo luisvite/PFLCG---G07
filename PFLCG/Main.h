8275523ba74dd8e353a1fc5fadbab3491641f1bb
Dark-Angel07 <36516433+Dark-Angel07@users.noreply.github.com>
2019-05-16 19:50:00 -0500

    edit
    trato de jalar el programa que pasaste

--- PFLCG/Main.h
+++ PFLCG/Main.h
@@ -1,7 +1,7 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
-//#include <windows.h>		// Archivo de cabecera para Windows
+//	// Archivo de cabecera para Windows
 #include <math.h>			// Archivo de cabecera para Funciones Matemáticas
 #include <stdio.h>			// Header File For Standard Input/Output
 #include <stdlib.h>			// Header File For Standard Library
@@ -9,7 +9,5 @@
 //#include "glu.h"			// Archivo de cabecera para la libreria GLu32
 //#include "glaux.h"			// Archivo de cabecera para la libreria Glaux
 //#include "GL\glut.h"			// Archivo de cabecera para la libreria Glut
-#include <GLUT/GLUT.h> // Archivo de cabecera para la libreria Glut
-#include <OpenGL/OpenGL.h>
 
 #endif 
